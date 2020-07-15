0e6c7fe20681c6041a1291cfc01977ffd1f28924
AnjalBam <anjalbam81@gmail.com>
2020-07-15 10:46:15 +0545

    bug fixed  and data got in matchContact inside PersonalInfo.js

--- src/Components/ContactProvider.js
+++ src/Components/ContactProvider.js
@@ -1,41 +1,36 @@
-import React, { Component } from 'react';
-import { Provider } from './Context';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import React, { Component } from "react";
+import { Provider } from "./Context";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class ContactProvider extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       contacts: [],
-      PersonalIfno: {}
-    }
+      PersonalIfno: {},
+    };
+    // this.OnClickHanlder = this.OnClickHanlder.bind(this);
   }
 
-  OnClickHanlder =  fullname =>{
-    const contacts = [...this.state.contacts];
-    const personalData = contacts.find(item => item.fullname === fullname);
-     this.setState({PersonalIfno: personalData})
+  componentDidMount() {
+    fetch("https://api.randomuser.me/?nat=us,gb&results=50")
+      .then(response => response.json())
+      .then(parsedResponse =>
+        parsedResponse.results.map(user => {
+          // console.log(user);
+          return {
+            fullname: `${user.name.title}-${user.name.first}-${user.name.last}`,
+            ...user,
+          };
+        })
+      )
+      .then(contact => {
+        return this.setState({ contacts: contact });
+      });
   }
 
-  componentDidMount(){
-    fetch('https://api.randomuser.me/?nat=us,gb&results=50')
-    .then(response => response.json())
-    .then(parsedResponse => parsedResponse.results.map(user => (
-      {
-        fullname: `${user.name.title} ${user.name.first} ${user.name.last}`,...user
-      }
-    )))
-    .then(contact => {
-		return this.setState({contacts: contact})
-	});
-  }
-
-  render(){
-    return(
-      <Provider value={{...this.state, OnClickHanlder: this.OnClickHanlder}} >
-        {this.props.children}
-      </Provider>)
+  render() {
+    return <Provider value={this.state}>{this.props.children}</Provider>;
   }
 }
 
